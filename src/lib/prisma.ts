69689cd0c374daf44e155a8a1548461010bf0b33
Sagar Gautam <sagargautam389@gmail.com>
2025-12-10 15:45:32 +0545

    upadte

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,18 @@
-import { PrismaClient } from '../generated/prisma/client'
+// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
